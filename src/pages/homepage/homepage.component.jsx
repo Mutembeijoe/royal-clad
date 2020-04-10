7e8a47c0b387fb7894ea520820953f3309316c82
mutembeijoe <mutembeijoe@gmail.com>
2020-04-10 11:08:43 +0300

    extracted Menu-Directory, Menu-Item components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss'
+import MenuDirectory from '../../components/menu-directory/menu-directory.component';
+
+const HomePage = ()=>(
+    <div className="homepage">
+       <MenuDirectory/>
+    </div>
+)
+
+
+export default HomePage;
