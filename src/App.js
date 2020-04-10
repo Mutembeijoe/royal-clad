cb0f86d4fe4e55db7e604d15190fd16d98c41a63
mutembeijoe <mutembeijoe@gmail.com>
2020-04-10 12:48:42 +0300

    adds routing to hats section page using react-router-dom withRouter HOC

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 
+const HatsPage = ()=>(
+  <h1>Hats Page</h1>
+)
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
       </Switch>
     </div>
   );
