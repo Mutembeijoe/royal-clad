0971a8e72236e7e37427e31798309c9879fc5016
mutembeijoe <mutembeijoe@gmail.com>
2020-04-15 08:25:05 +0300

    adds redux-persist to persist state to local storage :sparkles:

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
     
