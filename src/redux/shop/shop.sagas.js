faa5326ae63823c7f0cd87510bd587b9ab5163ca
mutembeijoe <mutembeijoe@gmail.com>
2020-04-23 08:56:41 +0300

    adds a root saga and switches takeEvery for takeLatest effect in fetching for collection data from db

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import {takeEvery, call, put } from 'redux-saga/effects';
+import {takeLatest, call, put } from 'redux-saga/effects';
 import shopActionTypes from './shop.types';
 import { firestore, convertCollectSnapshotToMap } from '../../firebase/firebase.utils';
 import { fetchCollectionSuccess, fetchCollectionFailure } from './shop.actions';
@@ -16,5 +16,5 @@ function* fetchCollectionAsync(){
 }
 
 export function* fetchCollectionStart(){
-   yield takeEvery(shopActionTypes.FETCH_COLLECTION_START, fetchCollectionAsync)
+   yield takeLatest(shopActionTypes.FETCH_COLLECTION_START, fetchCollectionAsync)
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
-// import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
-import { fetchCollectionStart } from "./shop/shop.sagas";
+import { rootSaga } from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -16,6 +15,6 @@ if (process.env.NODE_ENV === 'development'){
 
 export const store = createStore(rootReducer,applyMiddleware(...middlewares))
 
-sagaMiddleware.run(fetchCollectionStart)
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store)
